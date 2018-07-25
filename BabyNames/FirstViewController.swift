ef959dac641321f62f3e7617d04be3fe7b3889de
Tyler Lafferty <tylerlafferty4@gmail.com>
2018-07-25 12:26:54 -0400

    Initial Commit

---
+++ BabyNames/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BabyNames
+//
+//  Created by Tyler Lafferty on 7/25/18.
+//  Copyright © 2018 Tyler Lafferty. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BabyNames/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BabyNames
+//
+//  Created by Tyler Lafferty on 7/25/18.
+//  Copyright © 2018 Tyler Lafferty. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
